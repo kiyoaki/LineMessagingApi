d04026bc96a296969648d3730020c60733cd2168
Kiyoaki Tsurutani <kiyoaki.tsurutani@gmail.com>
2018-01-22 14:54:53 +0900

    WIP

---
+++ LineMessaging.Test/LineMessagingClientTest.cs
@@ -0,0 +1,31 @@
+using LINE;
+using SixLabors.ImageSharp;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LineMessaging.Test
+{
+    public class LineMessagingClientTest
+    {
+        private readonly LineMessagingClient apiClient;
+
+        public LineMessagingClientTest()
+        {
+            var accessToken = Environment.GetEnvironmentVariable("LINE_ACCESS_TOKEN");
+            apiClient = new LineMessagingClient(accessToken);
+        }
+
+        [Fact]
+        public async Task GetMessageContentTest()
+        {
+            const string messageId = "7349950403913";
+            var bytes = await apiClient.GetMessageContent(messageId);
+            Assert.NotNull(bytes);
+
+            var image = Image.Load(bytes);
+            Assert.NotNull(image);
+
+        }
+    }
+}
--- LineMessaging/Apis/LineMessageContentApi.cs
+++ LineMessaging/Apis/LineMessageContentApi.cs
@@ -7,14 +7,14 @@ namespace LINE
     {
         private const string MessageContentApiPath = "/v2/bot/message/{0}/content";
 
-        public async Task GetMessageContent(string messageId)
+        public Task<byte[]> GetMessageContent(string messageId)
         {
             if (string.IsNullOrEmpty(messageId))
             {
                 throw new ArgumentException($"{nameof(messageId)} is null or empty.");
             }
 
-            await GetAsByteArray(string.Format(MessageContentApiPath, messageId));
+            return GetAsByteArray(string.Format(MessageContentApiPath, messageId));
         }
     }
 }
